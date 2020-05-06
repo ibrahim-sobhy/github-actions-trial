d586579d80d288e3d42b1d1149637129674d23be
Ibrahim Sobhy <ibrahim.sobhy@apsis.com>
2020-05-06 09:57:27 +0200

    add change

--- src/test/java/com/trial/basic/HelloWorldTest.java
+++ src/test/java/com/trial/basic/HelloWorldTest.java
@@ -2,13 +2,13 @@ package com.trial.basic;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class HelloWorldTest {
 
     @Test
     public void simple() {
         new HelloWorld().sayHello("Ibrahim");
-        assertTrue(true);
+        fail();
     }
 }
